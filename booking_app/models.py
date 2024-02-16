df461d6a85751d235697e737df4743f93f6c1e1d
Kawarime <kaliak.miroslav@gmail.com>
2024-02-16 18:47:38 +0200

    Added_New_Models

--- booking_app/models.py
+++ booking_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    name = models.CharField(max_length=30)
+    surname = models.CharField(max_length=30)
+    email = models.EmailField(max_length=100)
+    birthdate = models.DateField()
+
+class Place(models.Model):
+    place = models.CharField(max_length=100)
+
+class Booking(models.Model):
+    user = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    ticket = models.ForeignKey(Place, on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    date = models.DateTimeField()
